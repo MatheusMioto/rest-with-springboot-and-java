06b9e4d2fd9a2ee00b58ed85a20e260ce15959e5
Matheus Mioto de Oliveira <matheusmiotto758@gmail.com>
2025-05-24 18:16:56 -0400

    Created MathController

---
+++ src/main/java/br/com/erudio/controllers/MathController.java
@@ -0,0 +1,19 @@
+package br.com.erudio.controllers;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/math")
+public class MathController {
+
+    @RequestMapping("/sum/{numberOne}/{numberTwo}")
+    public Double sum(
+            @PathVariable("numberOne") Double numberOne,
+            @PathVariable("numberTwo") Double numberTwo
+    ){
+        return numberOne + numberTwo;
+    }
+
+}
