332e6068840f897ce42febf87f7217e1db611e78
Matheus Mioto de Oliveira <matheusmiotto758@gmail.com>
2025-06-12 16:25:41 -0400

    Starts Person

---
+++ src/main/java/br/com/erudio/services/PersonServices.java
@@ -0,0 +1,28 @@
+package br.com.erudio.services;
+
+
+import br.com.erudio.model.Person;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+@Service
+public class PersonServices {
+
+    private final AtomicLong counter = new AtomicLong();
+
+    private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public Person findById(String id){
+        logger.info("Finding one Person!");
+
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Matheus");
+        person.setLastName("Mioto de Oliveira");
+        person.setAddress("Ariquemes - Rondonia - Brasil");
+        person.setGender("male");
+        return person;
+    }
+}
