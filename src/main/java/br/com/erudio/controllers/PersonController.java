718cba83f1ce281f4665a650f70a5619e3445f0a
Matheus Mioto de Oliveira <matheusmiotto758@gmail.com>
2025-06-25 18:20:08 -0400

    add core validators

--- src/main/java/br/com/erudio/controllers/PersonController.java
+++ src/main/java/br/com/erudio/controllers/PersonController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
 @RestController
 @RequestMapping("/api/person/v1")
 @Tag(name = "People", description = "Endpoints for Managing People")
