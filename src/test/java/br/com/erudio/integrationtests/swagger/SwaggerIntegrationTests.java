66d0343f7551bf58fd1ec089ae1a1169c94a3126
Matheus Mioto de Oliveira <matheusmiotto758@gmail.com>
2025-06-25 17:01:50 -0400

    add swaguer container test

---
+++ src/test/java/br/com/erudio/integrationtests/swagger/SwaggerIntegrationTests.java
@@ -0,0 +1,30 @@
+package br.com.erudio.integrationtests.swagger;
+
+import br.com.erudio.config.TestConfigs;
+import br.com.erudio.integrationtests.testcontainers.AbstractIntegrationTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertTrue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class SwaggerIntegrationTests extends AbstractIntegrationTest {
+
+	@Test
+	void shouldDisplaySwaggerUIPage() {
+		var content = given()
+				.basePath("/swagger-ui/index.html")
+					.port(TestConfigs.SERVER_PORT)
+				.when()
+					.get()
+				.then()
+					.statusCode(200)
+				.extract()
+					.body()
+						.asString();
+		Assertions.assertTrue(content.contains("Swagger UI"));
+	}
+
+}
