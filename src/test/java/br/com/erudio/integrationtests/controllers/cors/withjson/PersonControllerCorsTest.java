3f9091ee5b838968fb504e1f596fa5a51330fdad
Matheus Mioto de Oliveira <matheusmiotto758@gmail.com>
2025-08-14 16:19:47 -0400

    add json tests for person

--- src/test/java/br/com/erudio/integrationtests/controllers/cors/withjson/PersonControllerCorsTest.java
+++ src/test/java/br/com/erudio/integrationtests/controllers/cors/withjson/PersonControllerCorsTest.java
@@ -1,4 +1,4 @@
-package br.com.erudio.integrationtests.controllers.withjson;
+package br.com.erudio.integrationtests.controllers.cors.withjson;
 
 import br.com.erudio.config.TestConfigs;
 import br.com.erudio.integrationtests.dto.PersonDTOV1;
@@ -20,7 +20,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-class PersonControllerTest  extends AbstractIntegrationTest{
+class PersonControllerCorsTest extends AbstractIntegrationTest{
 
     private static RequestSpecification specification;
     private static ObjectMapper objectMapper;
@@ -74,6 +74,7 @@ class PersonControllerTest  extends AbstractIntegrationTest{
         assertEquals("Stallman", createdPerson.getLastName());
         assertEquals("New york", createdPerson.getAddress());
         assertEquals("Male", createdPerson.getGender());
+        assertTrue(createdPerson.getEnabled());
 
     }
     @Test
@@ -141,6 +142,7 @@ class PersonControllerTest  extends AbstractIntegrationTest{
         assertEquals("Stallman", createdPerson.getLastName());
         assertEquals("New york", createdPerson.getAddress());
         assertEquals("Male", createdPerson.getGender());
+        assertTrue(createdPerson.getEnabled());
     }
 
     @Test
@@ -186,5 +188,6 @@ class PersonControllerTest  extends AbstractIntegrationTest{
         person.setLastName("Stallman");
         person.setAddress("New york");
         person.setGender("Male");
+        person.setEnabled(true);
     }
 }
