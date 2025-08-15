3b26cf35d69ec3d4bba10b531ccc7be5a2f674e4
Matheus Mioto de Oliveira <matheusmiotto758@gmail.com>
2025-08-15 18:23:01 -0400

    Populate person data base

--- src/main/resources/db/migration/V2__Populate_Table_Person.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
-	(1, 'São Paulo - Brasil', 'Ayrton', 'Male', 'Senna'),
-	(2, 'Ariquemes - Brasil', 'Matheus', 'Male', 'Mioto de Oliveira');
--- src/main/resources/db/migration/V3__Create_Table_Person.sql
+++ src/main/resources/db/migration/V3__Create_Table_Person.sql
@@ -4,5 +4,6 @@ CREATE TABLE IF NOT EXISTS `person` (
   `last_name` varchar(80) NOT NULL,
   `address` varchar(100) NOT NULL,
   `gender` varchar(6) NOT NULL,
+  `enabled` BIT(1) NOT NULL DEFAULT b'1',
   PRIMARY KEY (`id`)
 );
